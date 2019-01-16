76c653dfe2a0e849ea178bb7cfd204f33cfe5c34
CSITEd Experts <csitedexperts@gmail.com>
2019-01-15 19:46:49 -0500

    HelloWorld program discussed

---
+++ src/HelloWorld.java
@@ -0,0 +1,32 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hello = "Hello World\nHow are you?\tWelcome to OOP";
+		System.out.println(hello);
+		double gpa = 3.5;
+		float price = 5.50f;
+
+		System.out.println(gpa);
+
+		System.out.println(price);
+
+		//	 char ch = 'A';
+		char ch = 100;
+
+		System.out.println(ch);
+
+		int x = 65;
+		System.out.println((char)x);  // A
+		System.out.println(x);  // 65
+		byte b = 120;
+		System.out.println(b);  // 65
+		
+		short s = 32569;
+		
+		long l = 723954;
+
+	}
+
+}
