bd59236e9b8a3fad35f01060a32202d6d7ed4221
CSITEd Experts <csitedexperts@gmail.com>
2019-01-22 19:46:33 -0500

    Vehicle class is added

---
+++ src/democlasses/Vehicle.java
@@ -0,0 +1,18 @@
+package democlasses;
+
+public class Vehicle {
+	
+	public int doors;
+	public int speed;
+	private String color;
+
+	public void run() {
+		// 
+
+	}
+
+	public Vehicle() {
+	//	System.out.println("A Vehicle object is created...");
+	}
+	
+}
---
+++ src/democlasses/VehicleMain.java
@@ -0,0 +1,26 @@
+package democlasses;
+
+public class VehicleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 Vehicle v1 = new Vehicle();
+		 Vehicle v2 = new Vehicle();
+		 Vehicle v3 = new Vehicle();
+		 Vehicle v4 = new Vehicle();
+		 v1.doors = 4;
+		 v2.doors = 2;
+		 v3.doors = 3;
+		 v1.speed = 120;
+		 v2.speed = 250;
+//		 v1.color = "Red";
+//		 v2.color = "White";
+//		 
+		 System.out.println("V1.doors = " + v1.doors);
+		 System.out.println("V2.doors = " + v2.doors);
+
+	
+	}
+
+}
