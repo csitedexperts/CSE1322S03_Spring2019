b7b8df65ea6090f1d987e756257ce530bbb8a52d
CSITEd Experts <csitedexperts@gmail.com>
2019-01-24 19:47:13 -0500

    Getter and Setter methods discussed

--- src/democlasses/Vehicle.java
+++ src/democlasses/Vehicle.java
@@ -1,18 +1,47 @@
 package democlasses;
 
 public class Vehicle {
-	
-	public int doors;
-	public int speed;
+	static int vehicleCount=0;
+	private int doors;
+	private int speed;
 	private String color;
 
-	public void run() {
-		// 
+	public int getDoors() { // Getter =>Get  ret
+		return doors;
+	}
 
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	public static void run() {
+		System.out.println("The run() called..");
+		
 	}
 
 	public Vehicle() {
 	//	System.out.println("A Vehicle object is created...");
+
+		vehicleCount++;
+		System.out.println("Vehicle(s) created: " + vehicleCount);	
 	}
+	
 	
 }
--- src/democlasses/VehicleMain.java
+++ src/democlasses/VehicleMain.java
@@ -2,25 +2,40 @@ package democlasses;
 
 public class VehicleMain {
 
+	public  void  Test() {
+		System.out.println("Test()  called");
+	
+		 Vehicle.run();
+			
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		 Vehicle v1 = new Vehicle();
 		 Vehicle v2 = new Vehicle();
-		 Vehicle v3 = new Vehicle();
-		 Vehicle v4 = new Vehicle();
-		 v1.doors = 4;
-		 v2.doors = 2;
-		 v3.doors = 3;
-		 v1.speed = 120;
-		 v2.speed = 250;
-//		 v1.color = "Red";
-//		 v2.color = "White";
+//		 Vehicle v3 = new Vehicle();
+//		 Vehicle v4 = new Vehicle();
+		 v1.setDoors(4);
+		 v2.setDoors(2);
+		 v1.setColor("Red");
+		 v2.setColor("White");
+		 
+		 System.out.println("v1 = " + v1);
+		 System.out.println("v2 = " + v2);
+		 
 //		 
-		 System.out.println("V1.doors = " + v1.doors);
-		 System.out.println("V2.doors = " + v2.doors);
+		 System.out.println("V1.doors = " + v1.getDoors());
+		 System.out.println("V1.color = " + v1.getColor());
 
+		 System.out.println("V2.doors = " + v2.getDoors());
+		 System.out.println("V2.color = " + v2.getColor());
+
+//		 Vehicle.run();
+//		 
+//		 VehicleMain.Test();
 	
 	}
 
+	// Why and how to override the toString() method?
+	
 }
